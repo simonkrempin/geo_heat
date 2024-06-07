d7fa9da81381a700cf7d1e92673059698f4cd228
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-06-07 11:59:11 +0200

    Remove duplicate code fragment

--- src/components/data-grid.tsx
+++ src/components/data-grid.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./data-grid.module.css";
 import { useLayerContext } from "@/context/layer-context";
 import { getColorByValue } from "@/utils/color-by-value";
@@ -7,17 +7,7 @@ export default function DataGrid() {
 	const { layerInformation } = useLayerContext();
 
 	const selectCountry = (country: string) => {
-		const elements = document.getElementsByClassName("rsm-geography");
-
-		let countryElement = null;
-		for (let i = 0; i < elements.length; i++) {
-			const element = elements[i] as HTMLElement;
-			const dataCountryName = element.getAttribute("data-countryname");
-			if (dataCountryName?.toLowerCase() === country.toLowerCase()) {
-				countryElement = element;
-				break;
-			}
-		}
+		const countryElement = getCountryElement(country);
 
 		if (countryElement) {
 			countryElement.style.fill = "rgb(136, 136, 136)";
@@ -25,6 +15,14 @@ export default function DataGrid() {
 	}
 
 	const deSelectCountry = (country: string) => {
+		const countryElement = getCountryElement(country);
+
+		if (countryElement) {
+			countryElement.style.fill = getColorByValue(layerInformation, country);
+		}
+	}
+
+	const getCountryElement = (country: string): HTMLElement | null => {
 		const elements = document.getElementsByClassName("rsm-geography");
 
 		let countryElement = null;
@@ -36,10 +34,8 @@ export default function DataGrid() {
 				break;
 			}
 		}
-		
-		if (countryElement) {
-			countryElement.style.fill = getColorByValue(layerInformation, country);
-		}
+
+		return countryElement;
 	}
 
 	if (!layerInformation) {
@@ -49,18 +45,18 @@ export default function DataGrid() {
 	return (
 		<table className={styles.table}>
 			<thead>
-				<tr>
-					<th className={styles.table__header_item}>Country</th>
-					<th className={styles.table__header_item}>Values</th>
-				</tr>
+			<tr>
+				<th className={styles.table__header_item}>Country</th>
+				<th className={styles.table__header_item}>Values</th>
+			</tr>
 			</thead>
 			<tbody>
-				{Object.entries(layerInformation.values).map(([country, value]) => (
-					<tr className={styles.table__entry} key={country} onMouseLeave={() => deSelectCountry(country)} onMouseOver={() => selectCountry(country)}>
-						<td >{country}</td>
-						<td >{value} {layerInformation.metadata.unit}</td>
-					</tr>
-				))}
+			{Object.entries(layerInformation.values).map(([country, value]) => (
+				<tr className={styles.table__entry} key={country} onMouseLeave={() => deSelectCountry(country)} onMouseOver={() => selectCountry(country)}>
+					<td >{country}</td>
+					<td >{value} {layerInformation.metadata.unit}</td>
+				</tr>
+			))}
 			</tbody>
 		</table>
 	);
