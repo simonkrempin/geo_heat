61e7403a979a24c41e414d9d43c48c897fa59d6d
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-06-07 13:20:37 +0200

    added base styling

--- src/components/layer-selection.tsx
+++ src/components/layer-selection.tsx
@@ -23,55 +23,63 @@ const LayerSelection: React.FC<LayerSelectionProps> = ({ children }) => {
 
 		setSearchSelected(false);
 	};
-	
+
 	const layersToDisplay = search === ""
 		? allLayers
 		: allLayers.filter((layer) => layer.toLowerCase().includes(search.toLowerCase()));
 
 	return (
 		<aside className={`${styles.selection} ${selectedLayer !== null ? styles.selection__active : ""}`}>
-			<div className={styles.selection__layers_container}>
+			<div style={{ width: "calc(100% - 40px)", position: "absolute", zIndex: 100, backgroundColor: "white" }}>
+				<div className={styles.selection__layers_container}>
+					<input
+						ref={inputRef}
+						className={styles.selection__search_bar}
+						onFocus={() => setSearchSelected(true)}
+						onBlur={handleBlur}
+						onChange={(e) => {
+							setSearch(e.target.value);
+							setSelectedLayer(null);
+						}}
+						value={search}
+						onKeyDown={(event) => {
+							if (event.key === "Enter" && layersToDisplay.length > 0) {
+								setSelectedLayer(layersToDisplay[0]);
+								setSearchSelected(false);
+								setSearch(layersToDisplay[0]);
+								inputRef.current?.blur();
+							}
+						}}
+					/>
+					{searchSelected &&
+                        <div
+                            className={styles.selection__layers}
+                            ref={layersRef}
+                        >
+							{layersToDisplay.map(layer => {
+								return (
+									<button
+										key={`${layer}-button`}
+										className={styles.selection__layers__button}
+										onClick={() => {
+											setSelectedLayer(layer);
+											setSearchSelected(false);
+											setSearch(layer);
+										}}
+									>
+										{layer}
+									</button>
+								);
+							})}
+                        </div>
+					}
+				</div>
 				<input
-					ref={inputRef}
-					className={styles.selection__search_bar}
-					onFocus={() => setSearchSelected(true)}
-					onBlur={handleBlur}
-					onChange={(e) => {
-						setSearch(e.target.value);
-						setSelectedLayer(null);
-					}}
-					value={search}
-					onKeyDown={(event) => {
-						if (event.key === "Enter" && layersToDisplay.length > 0) {
-							setSelectedLayer(layersToDisplay[0]);
-							setSearchSelected(false);
-							setSearch(layersToDisplay[0]);
-							inputRef.current?.blur();
-						}
-					}}
+					type={"range"}
+					min={0}
+					max={100}
+					style={{ padding: "10px 0", marginBottom: "10px", width: "100%" }}
 				/>
-				{ searchSelected && 
-					<div
-						className={styles.selection__layers}
-						ref={layersRef}
-					>
-						{layersToDisplay.map(layer => {
-							return (
-								<button
-									key={`${layer}-button`}
-									className={styles.selection__layers__button}
-									onClick={() => {
-										setSelectedLayer(layer);
-										setSearchSelected(false);
-										setSearch(layer);
-									}}
-								>
-									{layer}
-								</button>
-							);
-						})}
-					</div>
-				}
 			</div>
 			<div className={styles.selection__body}>
 				{children}
