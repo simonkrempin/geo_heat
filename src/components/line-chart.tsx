99f21f0660e43ec09f4e48e97577c70e785c4efb
Johannes <johannes.haick@gmail.com>
2024-06-28 14:40:06 +0200

    combine meta and line chart

--- src/components/line-chart.tsx
+++ src/components/line-chart.tsx
@@ -38,9 +38,29 @@ export default function LineChart() {
 		return null;
 	}
 
+	const calculateStats = () => {
+		const values = Object.entries(layerInformation?.values[clickedCountry] || {})
+			.filter(([year, value]) => value !== null)
+			.map(([year, value]) => ({ year, value }));
+
+		const min = Math.min(...values.map(obj => obj.value));
+		const minYear = values.find(obj => obj.value === min);
+
+		const max = Math.max(...values.map(obj => obj.value));
+		const maxYear = values.find(obj => obj.value === max);
+
+		const average = values.reduce((sum, { value }) => sum + value, 0) / values.length;
+		const biggestDifference = Math.max(...values.map(value => Math.abs(value.value - min)));
+		const differenceLastFirst = values[values.length - 1].value - values[0].value;
+		const span = values[0].year + " - " + values[values.length - 1].year;
+		return { minYear, maxYear, average, biggestDifference, differenceLastFirst, span };
+	};
+
+	const { minYear, maxYear, average, biggestDifference, differenceLastFirst, span } = calculateStats();
+
 	return (
 		<div
- 			style={{
+			style={{
 				justifyContent: "center",
 				position: "relative",
 				display: "flex",
@@ -50,9 +70,9 @@ export default function LineChart() {
 				borderRadius: "16px",
 				width: "50vw",
 				height: "80vh",
-                //top right bottom left
+				//top right bottom left
 				// padding: "200px 150px 200px 150px"
-			}} 
+			}}
 		>
 			<div>
 				<p
@@ -88,7 +108,7 @@ export default function LineChart() {
 					datasets: [
 						{
 							label: layerInformation.metadata.details,
-							data: Object.values(layerInformation?.values[clickedCountry] as Record<string, number>).filter(value => value!==0), // Data points for the line
+							data: Object.values(layerInformation?.values[clickedCountry] as Record<string, number>).filter(value => value !== 0), // Data points for the line
 							fill: false,
 							borderColor: "rgb(243,165,63)", // Line color
 							tension: 0.1, // Smoothening of the line
@@ -103,5 +123,45 @@ export default function LineChart() {
 					},
 				}}
 			/>
+			<div
+				style={{ color: "black" }}
+			>
+				<table>
+					<thead>
+						<tr>
+							<th></th>
+							<th>Year</th>
+							<th>Value</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Max</td>
+							<td>{maxYear?.year}</td>
+							<td>{maxYear?.value.toFixed(2)}</td>
+						</tr>
+						<tr>
+							<td>Min</td>
+							<td>{minYear?.year}</td>
+							<td>{minYear?.value.toFixed(2)}</td>
+						</tr>
+						<tr>
+							<td>Average</td>
+							<td></td>
+							<td>{average.toFixed(2)}</td>
+						</tr>
+						<tr>
+							<td>Difference between last and first value</td>
+							<td>{span}</td>
+							<td>{differenceLastFirst.toFixed(2)}</td>
+						</tr>
+						<tr>
+							<td>Biggest difference</td>
+							<td>{maxYear?.year + " - " + minYear?.year}</td>
+							<td>{biggestDifference.toFixed(2)}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>);
 }
